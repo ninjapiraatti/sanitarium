00b79e389f667da1bf1a308526de719a2ebbfe8a
Tuomas Louekari <tuoppil@gmail.com>
2024-07-15 16:40:59 +0300

    Actually make aninmal component work with async call and useeffect

--- app/components/aninmal.tsx
+++ app/components/aninmal.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Post } from "@/types/post"
 import { getPostAuthor } from "@/sanity/sanity-utils"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 type Props = {
   aninmal: Post
@@ -9,8 +9,13 @@ type Props = {
 
 export const Aninmal = (props: Props) => {
   const [sound, setSound] = useState("meow");
+  const [author, setAuthor] = useState("authorrrrr");
   const authorId = props.aninmal.author?._ref
-  const author = async () => await getPostAuthor(authorId)
+  const fetchAuthor = async () => {
+    const author = await getPostAuthor(authorId)
+    author ? setAuthor(author.name) : setAuthor("autototo")
+  }
+  useEffect(() => { fetchAuthor() })
 
   function makeSound() {
     setSound("woof");
@@ -19,11 +24,11 @@ export const Aninmal = (props: Props) => {
   return (
     <>
       <h1>{props.aninmal.title}</h1>
-      <p>{author?.name}</p>
+      <p>{author}</p>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={makeSound}>
         Make sound!
       </button>
-      <p>{props.aninmal.title} says {sound}!</p>
+      <p className="mb-4">{props.aninmal.title} says {sound}!</p>
     </>
   )
 }
